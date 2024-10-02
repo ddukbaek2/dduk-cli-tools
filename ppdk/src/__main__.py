08fe69977c386dbf59dcf52b2535ffaa60375a82
kwansikjung <kwansik.jung@altava.com>
2024-10-02 20:20:39 +0900

    작업중.

--- ppdk/src/__main__.py
+++ ppdk/src/__main__.py
@@ -4,6 +4,7 @@
 from __future__ import annotations
 from typing import Any, Final, Callable, Iterator, Optional, Type, TypeVar, Union, Tuple, List, Dict, Set, cast
 import builtins
+from argparse import ArgumentParser
 import sys
 from dduk.application.predefinedsymbols import PredefinedSymbols
 from dduk.application.application import Application
@@ -16,7 +17,13 @@ import ppdk
 def Main(arguments : list[str]) -> int:
 	# 첫번째 인수 실행 파일 경로는 제외.
 	try:
-		return ppdk.Main(arguments)
+		# return ppdk.Main(arguments)
+		parser = ArgumentParser()
+		parser.add_argument("command")
+		parser.add_argument("-p", "--path", default = "")
+		parser.add_argument("-i", "--interpreter", default = "")
+		parser.add_argument("arguments", nargs = '*', default = list())
+		return ppdk.Main(parser)
 	except Exception as exception:
 		Application.LogException(exception)
 		Application.Exit(1)
--- ppdk/src/ppdk/__main__.py
+++ ppdk/src/ppdk/__main__.py
@@ -4,15 +4,60 @@
 from __future__ import annotations
 from typing import Any, Final, Callable, Iterator, Optional, Type, TypeVar, Union, Tuple, List, Dict, Set, cast
 import builtins
+from argparse import ArgumentParser, Namespace
+import subprocess
 import sys
 from dduk.application.predefinedsymbols import PredefinedSymbols
 from dduk.application.application import Application
-import argparse
+
+
+def GetInstalledPythonInterpreters() -> list[str]:
+	try:
+		if sys.platform == "win32":
+			output : str = subprocess.check_output("where python", shell = True, text = True)
+		else:
+			output : str = subprocess.check_output("which -a python python3", shell = True, text = True)
+		return output.strip().split("\n")
+	except Exception as exception:
+		return list()
 
 
 #--------------------------------------------------------------------------------
 # 패키지 메인 함수.
 #--------------------------------------------------------------------------------
-def Main(arguments : list[str]) -> int:
-	Application.Log("pdk")
+def Main(parser : ArgumentParser) -> int:
+	parsedArguments : Namespace = parser.parse_args()
+	command : str = parsedArguments.command
+	path : str = parsedArguments.path
+	arguments : list[str] = parsedArguments.arguments
+
+	for pythonInterpreter in GetInstalledPythonInterpreters():
+		builtins.print(pythonInterpreter)
+
+	if command == "new": # p, i
+		interpreter : str = parsedArguments.interpreter
+		if not interpreter:
+			
+		builtins.print(interpreter)
+		pass
+	elif command == "update":
+		pass
+	elif command == "build":
+		pass
+	elif command == "archive":
+		pass
+	elif command == "run": # p a
+		pass
+	elif command == "tests":
+		pass
+	elif command == "debug": # p a
+		pass
+	elif command == "service": # p a
+		pass
+	else:
+		builtins.print(command)
+		builtins.print(path)
+		builtins.print(interpreter)
+		builtins.print(arguments)
+
 	return 0
